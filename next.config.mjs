647fec8cc10498c53aae4bb73365747bc6742a6c
JoeBryan <emekejoshua12@gmail.com>
2025-03-15 04:46:09 -0700

    headers update

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,28 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-control-allow-origin",
+            value: "*",
+          },
+          {
+            key: "Access-control-allow-methods",
+            value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          },
+          {
+            key: "Access-control-allow-headers",
+            value:
+              "Content-Type, Authorization, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Credentials",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
